3ee7b77c72e0b765a1bcf17c0ead88f5f4f5abcb
Peter Loux <prl83@msstate.edu>
2023-05-10 10:31:13 -0500

    make dictionary function

--- 02_maketext/sentence.py
+++ 02_maketext/sentence.py
@@ -3,6 +3,26 @@ import datetime
 
 geneSet = "abcdefghijklmnopqrstuvwxyz'"
 
+def makedict(file, newfile=False):
+    # this function takes a dictionary and creates a set of all the words
+    # newfile is a boolean that determines whether or not to create a new file
+    # open the file
+    with open(file, 'r') as f:
+        # read the file
+        text = f.read()
+        # split the text into a list of words
+        words = text.split()
+        # create a set of the words
+        wordset = set(words)
+        # if newfile is true
+        if newfile:
+            # create a new file
+            with open('dict.txt', 'w') as f:
+                # write the wordset to the file
+                f.write(str(wordset))
+        # return the wordset
+        return wordset
+
 # function to generate random sentences
 def sentences(num, words, letters):
     # initialize the list of sentences
@@ -27,7 +47,7 @@ def sentences(num, words, letters):
     return sentences
 
 # function to calculate the fitness of a sentence
-def fitness(sentence, target):
+# def fitness(sentence, target):
 
 # function to display the list of sentences
 def display(sentences):
@@ -41,5 +61,10 @@ def display(sentences):
     
 test_sentences = sentences(5, 10, 5)
 display(test_sentences)
+
+# create a wordset and print the first 100 words, the length, and save it to a file
+wordset = makedict("02_maketext/american-english.txt", True)
+print(list(wordset)[:100])
+print(len(wordset))
 
     
